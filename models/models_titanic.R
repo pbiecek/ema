a8dfef18527bd6f78257a7651ac3f1a73d095ed4
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-03-07 17:43:16 +0100

    updated hooks to explainers created with DALEX v1.0

--- models/models_titanic.R
+++ models/models_titanic.R
@@ -3,33 +3,63 @@ library("randomForest")
 library("gbm")
 library("e1071")
 library("rms")
-titanic <- na.omit(titanic)
+library("ggplot2")
+library("ggmosaic")
+library("patchwork")
+library("forcats")
 
 link_to_models <- "models/models_titanic.rda"
+
+titanic <- archivist::aread("pbiecek/models/27e5c")
+
 
 if (file.exists(link_to_models)) {
   load(link_to_models)
 } else {
+  # here we create models and explainers
+  # rms
+  set.seed(1313)
+  model_titanic_lmr <- lrm(survived == "yes" ~ gender + rcs(age) + class + sibsp +
+                          parch + fare + embarked, titanic)
 
-  #here we create models and explainers
-  library("randomForest")
-  model_titanic_rf <- randomForest(survived == "yes" ~ gender + age + class + embarked +
-                                     fare + sibsp + parch,  data = titanic)
-  explain_titanic_rf <- explain(model_titanic_rf, 
-                                data = titanic[,-9],
-                                y = titanic$survived == "yes",
-                                label = "Random Forest")
+  explain_titanic_lmr <- explain(model = model_titanic_lmr, 
+                                    data = titanic[, -9],
+                                    y = titanic$survived == "yes", 
+                                    label = "Logistic Regression")
   
-  library("gbm")
-  model_titanic_gbm <- gbm(survived == "yes" ~ class + gender + age + sibsp +
-                             parch + fare + embarked, data = titanic, n.trees = 15000)
+  # random forest
+  set.seed(1313)
+  model_titanic_rf <- randomForest(survived ~ class + gender + age + sibsp + parch + fare + embarked, 
+                                data = titanic)
+  model_titanic_rf
   
-  explain_titanic_gbm <- explain(model_titanic_gbm, data = titanic, 
-                                 y = titanic$survived == "yes", 
-                                 predict_function = function(m,x) predict(m, x, n.trees = 15000, type = "response"),
-                                 label = "Generalized Boosted Model")
+  explain_titanic_rf <- explain(model = model_titanic_rf, 
+                                   data = titanic[, -9],
+                                   y = titanic$survived == "yes", 
+                                   label = "Random Forest")
   
-  library("e1071")
+  model_titanic_rf_small <- randomForest(survived ~ class + gender + age, data = titanic)
+  model_titanic_rf_small
+  
+  explain_titanic_rf_small <- explain(model = model_titanic_rf_small, 
+                                   data = titanic[, -9],
+                                   y = titanic$survived == "yes", 
+                                   label = "Random Forest small")
+  
+  
+  # gbm
+  set.seed(1313)
+  model_titanic_gbm <- gbm(survived == "yes" ~ class + gender + age + sibsp + parch + fare + embarked, 
+                        data = titanic, n.trees = 15000, distribution = "bernoulli")
+  model_titanic_gbm
+  
+  explain_titanic_gbm <- explain(model = model_titanic_gbm, 
+                                    data = titanic[, -9],
+                                    y = titanic$survived == "yes", 
+                                    label = "Generalized Boosted Regression")
+  
+  # svm
+  
   model_titanic_svm <- svm(survived == "yes" ~ class + gender + age + sibsp +
                              parch + fare + embarked, data = titanic, 
                            type = "C-classification", probability = TRUE)
@@ -37,14 +67,9 @@ if (file.exists(link_to_models)) {
                                  y = titanic$survived == "yes", 
                                  label = "Support Vector Machines")
   
-  library("rms")
-  model_titanic_lmr <- lrm(survived == "yes" ~ class + gender + rcs(age) + sibsp +
-                             parch + fare + embarked, titanic)
-  explain_titanic_lmr <- explain(model_titanic_lmr, data = titanic, 
-                                 y = titanic$survived == "yes", 
-                                 predict_function = function(m,x) predict(m, x, type="fitted"),
-                                 label = "Logistic regression")
+  
   save(model_titanic_rf, explain_titanic_rf, 
+       model_titanic_rf_small, explain_titanic_rf_small, 
        model_titanic_svm, explain_titanic_svm, 
        model_titanic_gbm, explain_titanic_gbm, 
        model_titanic_lmr, explain_titanic_lmr, 
