6d0029316fdccb5de8e81a130f659bea940d6f34
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-03-07 20:34:20 +0100

    updated archivist links to objects

--- models/models_apartments.R
+++ models/models_apartments.R
@@ -10,5 +10,33 @@ if (file.exists(link_to_models)) {
   load(link_to_models)
 } else {
 
+  # lm
+  apartments_lm_v5 <- lm(m2.price ~ ., data = apartments)
+  model_apartments_lm <- apartments_lm_v5
+  explain_apartments_lm_v5 <- explain(model = apartments_lm_v5, 
+                                      data = apartments_test,
+                                      y = apartments_test$m2.price,
+                                      label = "Linear Regression")
+  
+  # random forest
+  set.seed(72)
+  apartments_rf_v5 <- randomForest(m2.price ~ ., data = apartments)
+  explain_apartments_rf_v5 <- explain(model = apartments_rf_v5, 
+                                      data = apartments_test,
+                                      y = apartments_test$m2.price,
+                                      label = "Random Forest")
+  
+  # svm
+  apartments_svm_v5 <- svm(m2.price ~ construction.year + surface + floor + 
+                             no.rooms + district, data = apartments)
+  explain_apartments_svm_v5 <- explain(model = apartments_svm_v5, 
+                                       data = apartments_test,
+                                       y = apartments_test$m2.price,
+                                       label = "Support Vector Machines")
+  
 }
+
+model_apartments_lm <- apartments_lm_v5
+model_apartments_rf <- apartments_rf_v5
+model_apartments_svm <- apartments_svm_v5
 
