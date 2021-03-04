112277b4a5e17d9b587e8a6ab18c33c9dc762dec
Hubert Baniecki <hbaniecki@gmail.com>
2021-03-04 23:14:03 +0100

    update modelStudio dashboards to v2.1.1

--- modelStudio/generateModelStudio.R
+++ modelStudio/generateModelStudio.R
@@ -15,21 +15,25 @@ new_obs <- rbind(johny_d = johny_d, henry = henry)
 # restore models for titanic
 
 titanic_lmr_v6 <- archivist::aread("pbiecek/models/51c50")
+titanic_lmr_v6$model_info$type <- "classification"
 ms <- modelStudio(titanic_lmr_v6, new_obs)
 r2d3::save_d3_html(ms, file = "titanic_lmr_v6.html", selfcontained = TRUE, 
                    title = "modelStudio for titanic_lmr_v6 and titanic")
 
 titanic_rf_v3 <- archivist::aread("pbiecek/models/0e5d2")
+titanic_rf_v3$model_info$type <- "classification"
 ms <- modelStudio(titanic_rf_v3, new_obs)
 r2d3::save_d3_html(ms, file = "titanic_rf_v3.html", selfcontained = TRUE, 
                    title = "modelStudio for titanic_rf_v3 and titanic")
 
 titanic_gbm_v6 <- archivist::aread("pbiecek/models/3d514")
+titanic_gbm_v6$model_info$type <- "classification"
 ms <- modelStudio(titanic_gbm_v6, new_obs)
 r2d3::save_d3_html(ms, file = "titanic_gbm_v6.html", selfcontained = TRUE, 
                    title = "modelStudio for titanic_gbm_v6 and titanic")
 
 titanic_rf_v6 <- archivist::aread("pbiecek/models/9b971")
+titanic_rf_v6$model_info$type <- "classification"
 ms <- modelStudio(titanic_rf_v6, new_obs)
 r2d3::save_d3_html(ms, file = "titanic_rf_v6.html", selfcontained = TRUE, 
                    title = "modelStudio for titanic_rf_v6 and titanic")
